003ff3e319981b80f9dd858cd78cb61c823a9e0a
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-04-16 16:49:56 -0700

    Quiz Displayed and Answers Checked.

--- scripts/script.js
+++ scripts/script.js
@@ -1,179 +1,146 @@
-let buttonBegin = $('#begin-ID');
-let questionText = $('.question-text');
+// Buttons
+let btnBegin = document.querySelector('.begin');
+let btnNextQuestion = document.querySelector('.next');
+let btnSubmit = document.querySelector('.submit');
+let btnClose = document.querySelector('.close');
 
-buttonBegin.on('click', startQuiz);
+// Content Areas
+let quizContainer = document.querySelector('.quiz');
+let quizResults = document.querySelector('.results');
 
-function startQuiz() {
-    questionText.text("<p>Testing</p>");
-    console.log(questionText);
+// Event Listeners
+// btnBegin.addEventListener('click', createQuiz);
+btnSubmit.addEventListener('click', displayResults);
+
+// Input + Output
+// let output= [];
+// let choices = [];
+let currentQuestion = '';
+let questionNumber = '';
+
+// Question + Answer Array
+let quizContent = [
+    {
+    question: "Question1",
+        choices: {
+        a: "Q1a",
+        b: "Q1b",
+        c: "Q1c",
+        d: "Q1d"
+        },
+    answer: "Q1a"
+    },
+    {
+    question: "Question2",
+        choices: {
+        a: "Q2a",
+        b: "Q2b",
+        c: "Q2c",
+        d: "Q2d"
+        },
+    answer: "Q2a"
+    },
+    {
+    question: "Question3",
+        choices: {
+        a: "Q3a",
+        b: "Q3b",
+        c: "Q3c",
+        d: "Q3d"
+        },
+    answer: "Q3a"
+    },
+    {
+    question: "Question4",
+        choices: {
+        a: "Q4a",
+        b: "Q4b",
+        c: "Q4c",
+        d: "Q4d"
+        },
+    answer: "Q4a"
+    },
+    {
+    question: "Question5",
+        choices: {
+        a: "Q5a",
+        b: "Q5b",
+        c: "Q5c",
+        d: "Q5d"
+        },
+    answer: "Q5a"
+    },
+];
+
+// Functions
+
+createQuiz();
+
+function createQuiz(){
+    const output= [];
+    quizContent.forEach((currentQuestion, questionNumber) => {
+        const choices = [];
+        for(letter in currentQuestion.choices){
+        choices.push(`<label><input type="radio" name="question${questionNumber}" value="${letter}"> ${letter}: ${currentQuestion.choices[letter]}</label>`);
+        }
+        output.push(`<div class="slide"><div class="question"> ${currentQuestion.question} </div><div class="choices"> ${choices.join('')} </div></div>`);
+    });
+    
+    quizContainer.innerHTML = output.join('');
 };
 
+function displayResults(){
+    const answerContainers = quizContainer.querySelectorAll('.choices');
+    let numCorrect = 0;
+
+    quizContent.forEach( (currentQuestion, questionNumber) => {
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        if(userAnswer === currentQuestion.answer){
+            numCorrect++;
+            answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        else{
+            answerContainers[questionNumber].style.color = 'red';
+        }
+        });
+
+        resultsContainer.innerHTML = `${numCorrect} out of ${quizContent.length}`;
+};
+
+
+
+
+// Attaches event listener to close button
+closeButton.addEventListener("click", resetGame);
+
+// ---------------------------------------------------------------
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
 
 
-// let buttonSubmit = $('#submit');
-// let buttonRadio = $('[name="userAnswer"]');
+//   document.getElementById("countdown-Timer").innerHTML = "";
 
 
 
-// let quiz = $("#quiz");
-// let validationAnswer = $("#validation");
-// let scoreCurrent = $("#current-score");
-// let progressBar = $("#my-bar");
 
-// let quizContent = [];
-// let answers = [];
-// let correctAnswer = [];
 
-// let questionNumber;
-// let question;
-// let answers;
-// let correctAnswer;
-
-// //Question Array
-// let quizContent = [
-//     {
-//     questionNumber: "1",
-//     question: "Question1",
-//         answers: {
-//         a: "Q1a",
-//         b: "Q1b",
-//         c: "Q1c",
-//         d: "Q1d"
-//         },
-//         correctAnswer: "Q1a"
-//     },
-//     {
-//     questionNumber: "2",
-//     question: "Question2",
-//         answers: {
-//         a: "Q2a",
-//         b: "Q2b",
-//         c: "Q2c",
-//         d: "Q2d"
-//         },
-//         correctAnswer: "Q2a"
-//     },
-//     {
-//     questionNumber: "3",
-//     question: "Question3",
-//         answers: {
-//         a: "Q3a",
-//         b: "Q3b",
-//         c: "Q3c",
-//         d: "Q3d"
-//         },
-//         correctAnswer: "Q3a"
-//     },
-//     {
-//     questionNumber: "4",
-//     question: "Question4",
-//         answers: {
-//         a: "Q4a",
-//         b: "Q4b",
-//         c: "Q4c",
-//         d: "Q4d"
-//         },
-//         correctAnswer: "Q4a"
-//     },
-//     {
-//     questionNumber: "5",
-//     question: "Question5",
-//         answers: {
-//         a: "Q5a",
-//         b: "Q5b",
-//         c: "Q5c",
-//         d: "Q5d"
-//         },
-//         correctAnswer: "Q5a"
-//     },
-// ];
 
 // ---------------------------------------------------Start Functions --------------------------------- //
-//VAR
-
-
-//Starts Quiz when the Begin button is clicked
-
-    // isCorrect = false;
-    // timerCount = 500000;
-    // Prevents start button from being clicked when round is in progress
-    // buttonBegin.disabled = true;
-    // score();
-    // startTimer();
-//     presentQuestions;
-// };
-
-// function presentQuestions() {
-    
-
-    // $('.question-text').text("<span>Testing</span>");
-
-// for (var i = 0; i < quizContent.length; i++) {
-
-// }};
-        // Replace QuestionText Field with next question.
-        // $('.question-text').replaceWith(question-text.text(question.each);
-
-
-
-        // $('.question-text').replaceWith(question-text.text(question.each);
-        //  question.each = (question-text.text(question.each);
-
-
-
-/*
-//Question: Should I do this on the form or button? Want to console-log true/not true...we'll get there.
-buttonSubmit.on('click', function(event) {
-    console.log("Saving Answer", form.elements.userAnswer.value);
-    event.preventDefault();
-});
-
-function correctAnswer() {
-    var val= "";
-    for (var i = 0, length = buttonRadio.length; i < length; i++) {
-        if (buttonRadio[i].checked) {
-        val = buttonRadio[i].value; 
-        break;
-        }
-    }
-
-    if (val == "") {
-        alert('Please Select an Answer');
-        } else if ( val == "(HOW TO INPUT/textContent or value?)" ) {
-        alert('Correct');
-        correctAnswer++;
-        } else {
-        alert('Incorrect');
-        timerCount--;
-        }
-};
-
-
-buttonRadio.each(function() {
-        console.log( this.value + ":" + this.checked );
-    });
-
-
-// TO DO: Call when Start Quiz is initiated via the Begin button
-function score() {
-    getCorrectAnswer();
-    getIncorrectAnswer();
-}
 
 
 //Scoring. 
-let sumArray = function(arr) {
-    let result = 0;
-    for (let iScore = 0; iScore < arr.length; iScore++) {
-    let currentNumber = arr[iScore];
-    result += currentNumber;
-    }
+// let sumArray = function(arr) {
+//     let result = 0;
+//     for (let iScore = 0; iScore < arr.length; iScore++) {
+//     let currentNumber = arr[iScore];
+//     result += currentNumber;
+//     }
 
-    return result;
-};
+//     return result;
+// };
 
 
 // Timer
@@ -226,5 +193,4 @@ function move() {
         }
     }
 }
-// Attaches event listener to button
-resetButton.addEventListener("click", resetGame);
+
