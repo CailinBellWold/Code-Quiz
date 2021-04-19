f355d1715278c538b4779e556d22804fd24922be
Cailin Bell Wold <79965490+CailinBellWold@users.noreply.github.com>
2021-04-18 21:04:51 -0700

    Updating Logic Structure to Avoid Looping Error.

--- scripts/script.js
+++ scripts/script.js
@@ -1,112 +1,77 @@
-// Buttons
-let btnBegin = document.querySelector('.begin');
-let btnModalButton = document.querySelector('.modal-button');
-let btnModalClose = document.querySelector('.close');
-
 // Content Areas
+
+// Modal/Quiz
 let quizContainerEl = document.querySelector('.quiz');
+// Modal/Question and Label
+let quizQuestionLabelEl = document.querySelector('.question-label')
 let quizCurrentQuestionEl = document.querySelector('#question');
+// Modal/Fieldset Label
+let quizSelectLabelEl = document.querySelector('.select-label')
+// Modal/Choices
+let quizChoiceEls = document.getElementsByName("choices");
 let quizCurrentChoiceAEl = document.querySelector('#choice-A');
 let quizCurrentChoiceBEl = document.querySelector('#choice-B');
 let quizCurrentChoiceCEl = document.querySelector('#choice-C');
 let quizCurrentChoiceDEl = document.querySelector('#choice-D');
+// Modal/Results Feedback
 let quizResultsEl = document.querySelector('#results');
+// Modal/Time Remaining
 let quizTimeRemainingEl = document.querySelector('#time');
-let quizQuestionLabelEl = document.querySelector('.question-label')
-let quizSelectLabelEl = document.querySelector('.select-label')
+// Modal/Fieldset 2
 let quizFieldsetEl = document.querySelector('.fieldset')
+//Modal/Score Form
 let quizScoreFormEl = document.querySelector('.score-form')
 let quizStaticScoreEl = document.querySelector('#static-score')
 
-let quizChoiceEl = document.getElementsByName("choices");
+// Buttons
+let btnBegin = document.querySelector('.begin');
+let btnModalButton = document.querySelector('.modal-button');
+let btnModalClose = document.querySelector('.close');
+let buttonState = 'Next'
 
 // Event Listeners
 btnBegin.addEventListener('click', setTime);
+btnModalButton.addEventListener('click', nextQuestion);
+for (let i = 0; i < quizChoiceEls.length; i++) {
+    const currentQuizChoiceEl = quizChoiceEls[i];
+    currentQuizChoiceEl.addEventListener('click', updateCurrentAnswer)
+};
 
 // Input + Output
 let numCorrect = 0;
+let score = 0;
+let userInitials = "";
 let currentQuestion = '';
 let currentChoiceA = ''; 
 let currentChoiceB = ''; 
 let currentChoiceC = ''; 
 let currentChoiceD = ''; 
-let i;
-let score = 0;
-let userInitials = "";
+let currentChoiceValue = '';
+
+// Index
+let currentQuestionIndex = 0;
 
 // Timer
 let secondsLeft = 60;
 let incorrectPenalty = 5;
 
 function setTime() {
-
     // Sets interval in variable
     let timerInterval = setInterval(function() {
     secondsLeft--;
     quizTimeRemainingEl.innerHTML = secondsLeft;
-
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
         // Alert Time's Up
         quizTimeRemainingEl.innerHTML = '<div class="font-weight-bold text-danger">Time\'s Up!</div>';
         quizComplete();
-    }
-
+    };
     }, 1000);
 
+    //Starts Quiz
     createQuiz();
 };
-
-// Function to Feed Questions to HTML
-function createQuiz() {
-for (i = 0; i < quizContentLength; i++) { 
-
-    currentQuestion = quizContent[i].question;
-    quizCurrentQuestionEl.innerHTML = currentQuestion;
-
-    currentChoiceA = quizContent[i].choices.a;
-    quizCurrentChoiceAEl.innerHTML = currentChoiceA;
-
-    currentChoiceB = quizContent[i].choices.b;
-    quizCurrentChoiceBEl.innerHTML = currentChoiceB;
-
-    currentChoiceC = quizContent[i].choices.c;
-    quizCurrentChoiceCEl.innerHTML = currentChoiceC;
-
-    currentChoiceD = quizContent[i].choices.d;
-    quizCurrentChoiceDEl.innerHTML = currentChoiceD;
-
-btnModalButton.addEventListener('click', scoreQuestion);
-};
-
-// function userResponse() {
-//     console.log('clicked');
-// };
-
-function scoreQuestion(event) {
-    // IF answered correctly + *not* the last question THEN numCorrect++, render next question
-    if ((quizChoiceEl.checked.value == quizContent[i].answer) && (quizContent[i] < lastQuestion)) {
-        quizResultsEl.innerHTML = '<div class="text-success">Correct!</div>';
-        numCorrect++;
-        // createQuiz();
-    // IF answered incorrectly correctly + *not* the last question THEN time penalty, render next question
-    } else if ((event.target.value != quizContent[i].answer) && (quizContent[i] < lastQuestion)) {
-        quizResultsEl.innerHTML = '<div class="text-warning">Incorrect</div>';
-        secondsLeft = secondsLeft - penalty;
-        // createQuiz();
-    // IF answered correctly + the last question THEN numCorrect++, quiz complete
-    } else if ((event.target.value = quizContent[i].answer) && (quizContent[i] = lastQuestion)) {
-            quizResultsEl.innerHTML = '<div class="text-success">Correct!</div>';
-            numCorrect++;
-            quizComplete();
-    // IF answered incorrectly + the last question THEN time penalty, quiz complete
-    } else {
-        quizResultsEl.innerHTML = '<div class="text-warning">Incorrect</div>';
-        secondsLeft = secondsLeft - penalty;
-        quizComplete();
-    }
-}};
 
 // Question + Answer Array
 let quizContent = [
@@ -164,6 +129,74 @@ let quizContent = [
 
 let quizContentLength = quizContent.length;
 let lastQuestion = quizContent.length - 1;
+
+function createQuiz() {
+    displayQuestion();
+};
+
+// Function to Feed Questions to HTML
+function displayQuestion() {
+    currentQuestion = quizContent[currentQuestionIndex].question;
+    quizCurrentQuestionEl.innerHTML = currentQuestion;
+
+    currentChoiceA = quizContent[currentQuestionIndex].choices.a;
+    quizCurrentChoiceAEl.innerHTML = currentChoiceA;
+
+    currentChoiceB = quizContent[currentQuestionIndex].choices.b;
+    quizCurrentChoiceBEl.innerHTML = currentChoiceB;
+
+    currentChoiceC = quizContent[currentQuestionIndex].choices.c;
+    quizCurrentChoiceCEl.innerHTML = currentChoiceC;
+
+    currentChoiceD = quizContent[currentQuestionIndex].choices.d;
+    quizCurrentChoiceDEl.innerHTML = currentChoiceD;
+
+    if (buttonState === 'End') {
+        btnModalButton.addEventListener('click', submitQuiz);
+    }
+};
+
+function updateCurrentAnswer(event) {
+    console.log(event);
+    currentChoiceValue = event.target.value;
+}
+
+function nextQuestion() {
+    scoreQuestion ();
+    if (currentQuestionIndex === quizContent.length - 1) {
+        buttonState = 'End';
+    };
+    if (buttonState !== 'End') {
+        setInterval(function() {
+            displayQuestion();
+        }, 5000);
+}
+
+function scoreQuestion(event) {
+    // IF answered correctly + *not* the last question, THEN numCorrect++, render next question
+    if ((currentChoiceValue == quizContent[currentQuestionIndex].answer) && (quizContent[currentQuestionIndex] < lastQuestion)) {
+        quizResultsEl.innerHTML = '<div class="text-success">Correct!</div>';
+        numCorrect++;
+        // createQuiz();
+    // IF answered incorrectly correctly + *not* the last question, THEN time penalty, render next question
+    } else if ((currentChoiceValue != quizContent[currentQuestionIndex].answer) && (quizContent[currentQuestionIndex] < lastQuestion)) {
+        quizResultsEl.innerHTML = '<div class="text-warning">Incorrect</div>';
+        secondsLeft = secondsLeft - penalty;
+        // createQuiz();
+    // IF answered correctly + the last question, THEN numCorrect++, quiz complete
+    } else if ((currentChoiceValue = quizContent[currentQuestionIndex].answer) && (quizContent[currentQuestionIndex] = lastQuestion)) {
+            quizResultsEl.innerHTML = '<div class="text-success">Correct!</div>';
+            numCorrect++;
+            quizComplete();
+    // IF answered incorrectly + the last question, THEN time penalty, quiz complete
+    } else {
+        quizResultsEl.innerHTML = '<div class="text-warning">Incorrect</div>';
+        secondsLeft = secondsLeft - penalty;
+        quizComplete();
+    }
+};
+
+
 
 function scoreQuiz() {
     clearInterval(timerInterval);
@@ -234,9 +267,4 @@ function quizComplete() {
 // ---------------------------------------------------------------
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
-})
-
-
-
-
-
+});};
